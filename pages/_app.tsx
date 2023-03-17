16dfe2930adb05a6e4db26a8b6c6a9dce33331ff
Marlon Costa <mrlnrnn@gmail.com>
2023-03-16 23:29:32 -0300

    Initial commit (probably the only one)

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import type {AppProps} from 'next/app'
+import {useState} from "react";
+import {createBrowserSupabaseClient} from "@supabase/auth-helpers-nextjs";
+import {SessionContextProvider} from "@supabase/auth-helpers-react";
+
+export default function MyApp({Component, pageProps}: AppProps) {
+    const [supabase] = useState(() => createBrowserSupabaseClient())
+
+    return (<SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
+        <Component {...pageProps} />
+    </SessionContextProvider>)
+}
